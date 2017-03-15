e2f27c8ce06ab1aaf6acb5fd9cff8d9ab56bb8f9
Michał Papierski <michal@papierski.net>
2017-03-15 11:03:22 +0100

    Initial bootstrap.
    You should be able to do `curl ... | bash` trick now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,93 @@
 #!/usr/bin/env bash
+# vim: set expandtab ts=4 sw=4:
 
 # A script to automate tasks
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+basic_packages=(
+    "git"
+    "vim"
+    "zsh"
+    "vim"
+    "tmux"
+    # Those are just for fun
+    "fortune"
+    "cowsay"
+)
+
+function _is_linux() {
+    [ "$(uname)" == "Linux" ];
+}
+
+function _is_ubuntu() {
+    _is_linux && [[ "$(cat /etc/issue)" =~ "Ubuntu"* ]];
+}
+
+function _is_macos() {
+    [[ "$(uname)" == "Darwin" ]];
+}
+
+function _check_status() {
+    if "$@" >/dev/null 2>&1; then
+        echo "✓";
+    else
+        echo "✗";
+    fi
+}
+
+function _prepare_ubuntu() {
+    echo -n "Update APT repositories... "
+    _check_status sudo apt-get update -qq
+}
+
+function _prepare_system() {
+    if _is_ubuntu; then
+        _prepare_ubuntu
+    fi
+}
+
+function _install_package() {
+    echo -n "Install $1... "
+    _check_status sudo apt-get install -qq -y $1
+}
+
+# The magic bootstrap
+function bootstrap() {
+    sudo echo "I am sudo!"
+    # This will keep updating user's timestamp
+    ( while true; do sudo -v; sleep 40; done >/dev/null) &
+    _pid=$!
+
+    # This is something like pre-bootstrap to make packages install
+    _prepare_system
+
+    # Install all basic packages
+    for package in "${basic_packages[@]}";
+    do
+        _install_package $package
+    done
+
+    # No need for `sudo` anymore
+    # TODO: This should be registered as trap signals
+    kill $_pid
+    sudo -k
+
+    # At this point we're supposed to have git installed already
+    # so lets proceed to cloning the repo
+    git clone --recursive https://github.com/mpapierski/dotfiles ~/.dotfiles
+
+    # Last final touch to make this script complete...
+    fortune | cowsay
+}
+
 function doIt() {
+    # Check if bootstrapped already
+    if [ ! -e ~/.dotfiles ]; then
+        # This will do the initial bootstrap
+        # with a bare minimum packages.
+        bootstrap;
+    fi
     rsync --exclude ".git/" \
           --exclude ".DS_Store" \
           --exclude ".gitmodules" \
